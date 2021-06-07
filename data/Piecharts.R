ffbd877e9ab062bd1c3020550431cd95fb56c882
anyakazachkova <anya.kazachkova98@gmail.com>
2021-06-07 12:27:48 +0300

    Piechart ChIPseek

---
+++ data/Piecharts.R
@@ -0,0 +1,40 @@
+source('lib.R')
+
+###
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+# BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+
+###
+
+#NAME <- 'H3K36me3_H9.intersect_with_DeepZ'
+#NAME <- 'DeepZ'
+#NAME <- 'H3K36me3_H9.ENCFF267AKB.h19.filtered'
+NAME <- 'H3K36me3_H9.ENCFF830UKK.h19.filtered'
+BED_FN <- paste0(NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+png(paste0('chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
+# 
+
+setwd('C:/Users/anyak/OneDrive/Documents/GitHub/hse21_H3K36me3_ZDNA_DeepZ_human/data')
